603915e3c5fb4480862742cd9815ae922214140d
Daniel Villalobos <daniel.villalobosdel@gmail.com>
2024-02-18 12:51:44 +0100

    Connect to CARLA. Listen for changes in topic. Write changes in topic

--- mqtt_communication/src/main.py
+++ mqtt_communication/src/main.py
@@ -1,31 +1,80 @@
+import asyncio
 import os
-import time
 import uuid
+import random
+import logging
+import carla
 from dotenv import load_dotenv
-from mqtt_node import DataManager, MQTTClient
+from mqtt_node.mqtt_client import MQTTClient
+from mqtt_node.data_manager import DataManager
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 load_dotenv()
 
-MQTT_HOST = os.getenv("MQTT_HOST") or "mqtt"
+MQTT_HOST = os.getenv("MQTT_HOST") or "localhost"
 MQTT_PORT = int(os.getenv("MQTT_PORT") or 1883)
+MQTT_TOPIC = os.getenv("MQTT_TOPIC") or "ros/data"
+CARLA_HOST = "localhost"
+CARLA_PORT = 2000
 
 
-def main():
-    broker_address = MQTT_HOST
-    port = MQTT_PORT
+async def carla_simulation(client_id: str, data_manager: DataManager):
+    global CARLA_HOST, CARLA_PORT
+    try:
+        client = carla.Client(CARLA_HOST, CARLA_PORT)
+        client.set_timeout(10.0)
+        logging.debug("Connected to Carla")
+        world = client.get_world()
+
+        blueprint_library = world.get_blueprint_library()
+        vehicle_bp = random.choice(blueprint_library.filter('vehicle.*'))
+        logging.debug(f"Selected vehicle blueprint: {vehicle_bp.id}")
+
+        spawn_point = random.choice(world.get_map().get_spawn_points())
+        vehicle = world.spawn_actor(vehicle_bp, spawn_point)
+        vehicle.set_autopilot(True)
+        logging.debug("Vehicle spawned")
+
+        while True:
+            new_data = {
+                "client_id": client_id,
+                "location": {"x": vehicle.get_location().x, "y": vehicle.get_location().y, "z": vehicle.get_location().z},
+                "rotation": {"yaw": vehicle.get_transform().rotation.yaw, "pitch": vehicle.get_transform().rotation.pitch, "roll": vehicle.get_transform().rotation.roll},
+                "acceleration": {"x": vehicle.get_acceleration().x, "y": vehicle.get_acceleration().y, "z": vehicle.get_acceleration().z},
+                "velocity": {"x": vehicle.get_velocity().x, "y": vehicle.get_velocity().y, "z": vehicle.get_velocity().z},
+                "angular_velocity": {"x": vehicle.get_angular_velocity().x, "y": vehicle.get_angular_velocity().y, "z": vehicle.get_angular_velocity().z}
+            }
+            data_manager.update_data(new_data)
+            await asyncio.sleep(1)
+
+    finally:
+        if vehicle:
+            vehicle.destroy()
+            print("Vehicle destroyed.")
+
+
+async def main():
+    global MQTT_HOST, MQTT_PORT, MQTT_TOPIC
+
+    logging.debug("Starting main")
     client_id = str(uuid.uuid4())
+    logging.info(f"Client ID: {client_id}")
 
-    mqtt_client = MQTTClient(broker_address, port, client_id)
+    mqtt_client = MQTTClient(MQTT_HOST, MQTT_PORT, client_id)
     mqtt_client.connect()
+    logging.debug("Connected to MQTT broker")
+    mqtt_client.subscribe(MQTT_TOPIC)
+    logging.debug(f"Subscribed to topic {MQTT_TOPIC}")
 
     data_manager = DataManager(mqtt_client)
-    counter = 0
-    while True:
-        new_data = {"client_id": client_id, "propiedad3": counter}
-        counter += 1
-        data_manager.update_data(new_data)
-        time.sleep(5)
+    logging.debug("DataManager initialized")
 
+    await asyncio.gather(
+        carla_simulation(client_id, data_manager),
+    )
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
--- mqtt_communication/src/mqtt_node/mqtt_client/mqtt_client.py
+++ mqtt_communication/src/mqtt_node/mqtt_client/mqtt_client.py
@@ -1,5 +1,6 @@
-import paho.mqtt.client as mqtt
 import json
+import logging
+import paho.mqtt.client as mqtt
 
 
 class MQTTClient:
@@ -8,19 +9,27 @@ class MQTTClient:
             mqtt.CallbackAPIVersion.VERSION2, client_id=client_id)
         self.host = host
         self.port = port
+        self.received_messages = []
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
+        self.client_id = client_id
 
     def on_connect(self, client, userdata, flags, rc, properties):
-        print(f"Conectado con código de resultado: {rc}")
+        logging.debug(f"Connected with result code {rc}")
 
     def on_message(self, client, userdata, message):
-        print(
-            f"Mensaje recibido: {message.payload.decode('utf-8')} en el topic {message.topic}")
+        data = json.loads(message.payload.decode('utf-8'))
+        if (data['client_id'] == self.client_id):
+            return
+        self.received_messages.append((message.topic, data))
+        logging.debug(f"Received message: {message.topic}")
 
     def connect(self):
         self.client.connect(self.host, self.port)
         self.client.loop_start()
+
+    def subscribe(self, topic):
+        self.client.subscribe(topic)
 
     def publish(self, topic, data):
         self.client.publish(topic, json.dumps(data))
